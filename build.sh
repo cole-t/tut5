d79090765bd30d7110dd52005f484d5ac7c00521
Cole Tustin <coletustin@gmail.com>
2018-09-17 20:57:49 +1200

    Removed extra spaces from script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/ bin / bash
-rm *. class # clean project
-javac Calc . java # compile project
+#!/bin/bash
+rm *.class # clean project
+javac Calc.java # compile project
 if [ $? -ne 0 ] # if exit code is not 0 javac failed
 then
 	echo " Compile failed !"
